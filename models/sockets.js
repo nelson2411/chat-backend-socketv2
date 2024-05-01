b23e2550eba96787ef9c96bd52c65c0a649c1d22
Nelson Rosales <nelson.rosales83@gmail.com>
2024-05-01 14:36:13 +0200

    Add socket controller for user connection and disconnection

--- models/sockets.js
+++ models/sockets.js
@@ -1,4 +1,5 @@
 const { verifyJWT } = require("../helpers/jwt")
+const { userConnected, userDisconnected } = require("../controllers/sockets")
 
 class Sockets {
   constructor(io) {
@@ -9,7 +10,7 @@ class Sockets {
 
   socketEvents() {
     // On connection
-    this.io.on("connection", (socket) => {
+    this.io.on("connection", async (socket) => {
       console.log(socket.handshake.query["x-token"])
       // Validate JWT at this very moment
       const [valid, uid] = verifyJWT(socket.handshake.query["x-token"])
@@ -19,16 +20,20 @@ class Sockets {
         console.log("Socket no identificado")
         return socket.disconnect()
       }
-      console.info("Client connected", uid)
 
       // Check which user is active (uid)
+      const user = await userConnected(uid)
+      console.info(`${user.name} connected`)
+
       // Emit all users connected
+
       // Socket join, uid
       // Listen when the client sends a message
       // personal message. this means that the message is only for the user who sent it
       // handling disconnection
       socket.on("disconnect", () => {
-        console.info("Client disconnected")
+        console.info(`${user.name} disconnected`)
+        userDisconnected(uid)
       })
       // flag the user as offline
       // Emit all users connected
